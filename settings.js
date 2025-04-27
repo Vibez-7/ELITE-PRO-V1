f3b09c4183a2354293fddfef7e8c5274c242a759
Vibez-7 <oluwavibez71@gmail.com>
2025-04-27 12:02:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348052287627"
+global.ownernomer = "2349162143581"
 global.ownername = "ElitePro"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
 
 
-global.ownernumber = '2348052287627'  //creator number
+global.ownernumber = '2349162143581'  //creator number
 global.ownername = 'ElitePro' //owner name
 global.botname = 'ELITE-PRO-V1' //name of the bot
 
